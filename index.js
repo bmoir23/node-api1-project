b17554f83d4f580bce52c686abd47175fb8e4ad2
bmoir23 <YOUR@EMAIL.com>
2020-01-16 21:59:52 -0500

    MVP

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express');
 const server = express();
 server.listen(4000, () => {
     console.log('------------server listening on port 4000....--------------');
-} );
+});
 
 server.use(express.json());
 
